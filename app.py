c8e01d2a0f85360a570c24b0d2b1409570b7ce27
Cheyenne <cheyennechoong@gmail.com>
2026-03-29 21:37:42 +0800

    Ribbon and project file creation.

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
+from process.project import Project
+from pathlib import Path
+
+projectEditor = Project()
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('index.html')
+    data = ""
+    with open(Path("process/project/project.txt").absolute(), "r") as file:
+        for line in file:
+            data = f"""
+            {data}
+            {line.strip("\n")}<br />
+            """
+    return render_template('index.html', projectList = data)
+
+@app.route('/newProject', methods=["POST"])
+def newProject():
+    projectName = request.form.get("fileName")
+    projectEditor.newProject(projectName.strip())
+    return redirect(url_for('home'))
+
+@app.route('/openProject', methods=["POST"])
+def openProject():
+    projectName = request.form.get("fileName")
+    projectEditor.openProject(projectName.strip())
+    print("Successfully Open")
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
---
+++ process/project.py
@@ -0,0 +1,48 @@
+"""
+project.py will act as the interface used in the app.py.
+This will connect all the backend processes and algorithms.
+"""
+
+import tkinter as tk
+from tkinter import filedialog
+import os
+from pathlib import Path
+import json
+
+class Project():
+    def __init__(self):
+        self.__path = ""
+    
+    def newProject(self, projectName:str):
+        __path = Path("process/project/project.txt")
+        if not __path:
+            return
+        
+        with open(str(__path.absolute()), 'r') as __file:
+            for __line in __file:
+                if __line.strip("\n") == projectName:
+                    return
+        
+        __path = Path("process/project")
+        with open(f"{str(__path.absolute())}/{projectName}.json", 'w') as __file:
+            __data = {
+                "title": projectName,
+                "names": [],
+                "floors": [],
+                "connect": [[], [], []]
+            }
+            json.dump(__data, __file, indent=4)
+        with open(f"{str(__path.absolute())}/project.txt", 'a') as __file:
+            __file.write(f"{projectName}\n")
+    
+    def openProject(self, projectName:str):
+        __path = Path("process/project/project.txt")
+        if not __path:
+            return
+        
+        with open(str(__path.absolute()), 'r') as __file:
+            __data = __file.readlines()
+        
+        if projectName in __data:
+            __path = Path("process/project")
+            self.__path = f"{__path}/{projectName}.json"
